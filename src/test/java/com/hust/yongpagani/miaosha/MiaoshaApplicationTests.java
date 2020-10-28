3f47e972ae17fa1c7792850e703bd597df488e2f
xiaoyonghu <2826755215@qq.com>
2020-10-28 21:06:46 +0800

    初始化秒杀仓库

---
+++ src/test/java/com/hust/yongpagani/miaosha/MiaoshaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hust.yongpagani.miaosha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiaoshaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
